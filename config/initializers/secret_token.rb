6ca00864387ac55e0a9d3b174511bd11d8cc85fe
Aaron Hay <haymaker13@hotmail.com>
2013-11-14 17:08:48 -0600

    Intial Commit of the ecomm store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcommStore::Application.config.secret_token = 'e0fe293296e32bc4cbbd87a175099a2dc850db9419c7adb09f1105bc910fcd8c850263fee99fdeff9174f9ece73d8a974f86115c2121dc8d7ddd1abdfa741c4b'
