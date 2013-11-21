8b15e90acc56a2d0e1be1200fe8397b1dbf7c894
Aaron Hay <haymaker13@hotmail.com>
2013-11-21 12:43:55 -0600

    changed layout of product interface and displayed images for products

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register Product do
-  
+  batch_action :flag do |selection|
+    Post.find(selection).each {|p| p.flag!  }
+    redirect_to collection_path, :notice => "Posts flagged!"
+  end
+  index do
+    column :name
+    column :category
+    column :company
+    column "Wheel Diameter", :diameter
+    column "Image" do |image|
+      image_tag(image.image, :size => "80x80")
+    end
+      
+    default_actions
+  end
 end
